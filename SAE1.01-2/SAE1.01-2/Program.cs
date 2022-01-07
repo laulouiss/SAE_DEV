46f5054a9a3a41bb3a873de3254cc290979b3305
Louis Laurent <Louis.Laurent@etu.univ-savoie.fr>
2022-01-07 10:40:16 +0100

    Creation du projet

---
+++ SAE1.01-2/SAE1.01-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SAE1._01_2
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
