1d319055b80eb240908170c0bd214a4fadc29963
Louis Laurent <Louis.Laurent@etu.univ-savoie.fr>
2022-01-10 10:15:34 +0100

    changement de dossier pour les images

--- SAE1.01-2/SAE1.01-2/Game1.cs
+++ SAE1.01-2/SAE1.01-2/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
 
 namespace SAE1._01_2
 {
@@ -8,6 +11,9 @@ namespace SAE1._01_2
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Vector2 _persoPosition;
+        private AnimatedSprite _perso;
+        private int _vitessePerso;
 
         public Game1()
         {
@@ -21,6 +27,8 @@ namespace SAE1._01_2
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            _persoPosition = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+            _vitessePerso = 220;
         }
 
         protected override void LoadContent()
@@ -28,6 +36,10 @@ namespace SAE1._01_2
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            // spritesheet
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("perso/tiled-perso-rouge.sf", new JsonContentLoader());
+            _perso = new AnimatedSprite(spriteSheet);
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,8 +48,15 @@ namespace SAE1._01_2
                 Exit();
 
             // TODO: Add your update logic here
-
+            string animation = "idle";
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds; // DeltaTime
+            float walkSpeed = deltaSeconds * _vitessePerso; // Vitesse de déplacement du sprite
+            KeyboardState keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Left) && (_persoPosition.X > 0 + _perso.TextureRegion.Width / 2))
+                animation = "walkWest";
             base.Update(gameTime);
+            _perso.Play(animation);
+            _perso.Update(deltaSeconds);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -45,7 +64,10 @@ namespace SAE1._01_2
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_perso, _persoPosition);
+            _spriteBatch.End();
+            base.Draw(gameTime);
             base.Draw(gameTime);
         }
     }
